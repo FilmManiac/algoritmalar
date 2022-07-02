762135c8eb90b13c04b77bdf2206f55aee53a083
FilmManiac <108547646+FilmManiac@users.noreply.github.com>
2022-07-02 15:21:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace algoritma_metotlar_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     int zaman1= DateTime.Now.Month;
+      Console.WriteLine(zaman1);
+     Console.ReadLine();
+}
